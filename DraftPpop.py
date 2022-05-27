116b3955a28cb83e3608311cf3cd4dfe90a345e1
cyberseihis <mcraftdm@gmail.com>
2022-05-27 18:51:37 +0300

    Lots of reworks, learns to climb if 0 punishment

--- DraftPpop.py
+++ DraftPpop.py
@@ -1,6 +1,6 @@
 from machin.frame.algorithms import PPO
 from machin.utils.logging import default_logger as logger
-from torch.distributions import Normal
+from torch.distributions import Normal, Uniform
 import torch as t
 import torch.nn as nn
 import gym
@@ -10,8 +10,7 @@ env = gym.make("MountainCarContinuous-v0")
 observe_dim = 2
 max_episodes = 1000
 max_steps = 999
-solved_reward = 190
-solved_repeat = 5
+solved_repeat = 3
 
 
 # model definition
@@ -27,7 +26,8 @@ class Actor(nn.Module):
         a = t.relu(self.fc1(state))
         a = t.relu(self.fc2(a))
         probs = t.sigmoid(self.fc3(a))
-        dist = Normal(probs[0][0], probs[0][1])
+        mu = probs[0][0] * 2 - 1
+        dist = Normal(mu, probs[0][1])
         act = action if action is not None else dist.sample()
         act_entropy = dist.entropy()
         act_log_prob = dist.log_prob(act.flatten())
@@ -49,14 +49,20 @@ class Critic(nn.Module):
         return v
 
 
-if __name__ == "__main__":
+def train():
     actor = Actor(observe_dim)
     critic = Critic(observe_dim)
 
-    ppo = PPO(actor, critic, t.optim.Adam, nn.MSELoss(reduction="sum"))
+    ppo = PPO(
+        actor, critic,
+        t.optim.Adam, nn.MSELoss(reduction="sum"),
+        actor_update_times=50, critic_update_times=100)
 
-    episode, step, reward_fulfilled = 0, 0, 0
+    episode, step = 0, 0
     smoothed_total_reward = 0
+    found = False
+    Scores = []
+    Dist = []
 
     while episode < max_episodes:
         episode += 1
@@ -64,19 +70,27 @@ if __name__ == "__main__":
         terminal = False
         step = 0
         state = t.tensor(env.reset(), dtype=t.float32).view(1, observe_dim)
-
+        changeRate = 5
+        unif = Uniform(-1, 1)
+        maxDist = -10
         tmp_observations = []
         while not terminal and step <= max_steps:
-            if episode % 100 == 0:
-                env.render()
+            # if episode % 100 == 0:
+            # env.render()
             step += 1
             with t.no_grad():
                 old_state = state
                 # agent model inference
-                action = ppo.act({"state": old_state})[0]
+                if step % changeRate == 1:
+                    if found:
+                        action = ppo.act({"state": old_state})[0]
+                    else:
+                        action = unif.sample()
                 state, reward, terminal, _ = env.step([action])
                 state = t.tensor(state, dtype=t.float32).view(1, observe_dim)
+                reward = max(0, reward)
                 total_reward += reward
+                maxDist = max(maxDist, state[0][0])
 
                 tmp_observations.append(
                     {
@@ -91,18 +105,23 @@ if __name__ == "__main__":
         # update
         ppo.store_episode(tmp_observations)
         if total_reward > 0:
-            ppo.update()
+            found = True
+            logger.info("LETS GO")
+        if found:
+            pass
+        elif episode > 20:
+            return episode
+        ppo.update()
 
         # show reward
         smoothed_total_reward *= 0.9
         smoothed_total_reward += total_reward * 0.1
-        Info = f"Episode {episode} total reward={smoothed_total_reward:.2f}"
+        Info = f"Ep {episode} >={maxDist:.2f} $={total_reward:.2f}"
         logger.info(Info)
+        Scores.append(total_reward)
+        Dist.append(maxDist)
 
-        if smoothed_total_reward > solved_reward:
-            reward_fulfilled += 1
-            if reward_fulfilled >= solved_repeat:
-                logger.info("Environment solved!")
-                exit(0)
-        else:
-            reward_fulfilled = 0
+
+if __name__ == "__main__":
+    while train() < 30:
+        pass
